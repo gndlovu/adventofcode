2c2422c81c37f9530676abcd48522e3243697a28
Gladwell Ndlovu <gladwell_n@live.com>
2023-12-14 13:50:34 +0200

    Day one part two

--- src/day-one/calibration-document-sum.js
+++ src/day-one/calibration-document-sum.js
@@ -17,6 +17,7 @@ const getData = (url) => {
     });
 };
 
+// Part 1
 (async (url) => {
     const data = await getData(url);
     const myArray = data.trim().split('\n');
@@ -25,6 +26,54 @@ const getData = (url) => {
         let firstNum = currentValue.charAt(0);
         let lastNum = currentValue.charAt(currentValue.length - 1);
         
+        return accumulator + parseInt(firstNum + lastNum);
+    }, 0);
+
+    console.log(sum)
+})('https://adventofcode.gladwelln.dev/day-one-input.txt');
+
+// Part 2
+(async (url) => {
+    const nums = {
+        one: 1, 
+        two: 2, 
+        three: 3, 
+        four: 4, 
+        five: 5, 
+        six: 6, 
+        seven: 7, 
+        eight: 8, 
+        nine: 9
+    };
+
+    const data = await getData(url);
+//     const data = `
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+//     `;
+    const myArray = data.trim().split('\n');
+    const sum = myArray.reduce((accumulator, currentValue) => {
+        let realFirstLast = {};
+        for (const key in nums) {
+            const index = currentValue.indexOf(key);
+            if (index >= 0) {
+                realFirstLast[index] = key;
+            }
+        }
+
+        for (const index in realFirstLast) {
+            currentValue = currentValue.replace(realFirstLast[index], nums[realFirstLast[index]]);
+        }
+    
+        currentValue = currentValue.replace(/\D/g, '');
+        let firstNum = currentValue.charAt(0);
+        let lastNum = currentValue.charAt(currentValue.length - 1);
+ 
         return accumulator + parseInt(firstNum + lastNum);
     }, 0);
 
